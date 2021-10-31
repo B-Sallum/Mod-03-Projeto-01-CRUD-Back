4cb310993024612cbcd8325e6d1ce329588126a4
B-Sallum <brusallum@hotmail.com>
2021-10-31 12:07:34 -0300

    simple CRUD and inserver DB working

--- routes/games.route.js
+++ routes/games.route.js
@@ -15,16 +15,16 @@ const gamesList = [
     name: "Super Mario World",
     category: "Platform",
     year: 1990,
-    imgURL: 'https://c.tenor.com/zXoyWTrUReUAAAAC/super-mario-bros-nintendo.gif',
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/pt/0/06/Super-Mario-World.jpg',
     havePlay: true,
     rating: 10
   },
   {
     id: pleseGiveMeAnId(),
-    name: "Zelda: Ocarina of Time",
+    name: "Zelda: A Link to the Past",
     category: "Action RPG",
-    year: 1998,
-    imgUrl: 'https://c.tenor.com/xaasBJILRhMAAAAd/zelda-oot.gif',
+    year: 1991,
+    imgUrl: 'https://1.bp.blogspot.com/-0_RMZLKFCac/X0ObUFOrZPI/AAAAAAAACzw/t9BEnzXfbXExcUracTEcc1fHTf5aG7WNQCLcBGAsYHQ/s2048/Zelda_SNES.jpg',
     havePlay: true,
     rating: 10
   },
@@ -33,7 +33,34 @@ const gamesList = [
     name: "Super Mario Kart",
     category: "Race Shooter",
     year: 1992,
-    imgUrl: 'https://c.tenor.com/oiGywgDXSx8AAAAC/mario-mario-kart.gif',
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/pt/f/fc/Super_Mario_Kart_front.jpg',
+    havePlay: true,
+    rating: 10
+  },
+  {
+    id: pleseGiveMeAnId(),
+    name: "Sonic: the Hedgehog",
+    category: "Racing Platform",
+    year: 1992,
+    imgUrl: 'https://www.sega-brasil.com.br/fullalbums/jogos/Mega%20Drive/Caixas%20de%20Plastico%20Preta/Sonic%20the%20Hedgehog/sonic_ft_c_zfm_sls.jpg',
+    havePlay: true,
+    rating: 10
+  },
+  {
+    id: pleseGiveMeAnId(),
+    name: "Donkey Kong Country",
+    category: "Platform",
+    year: 1994,
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/pt/8/83/Donkey_Kong_Country_capa.png',
+    havePlay: true,
+    rating: 10
+  },
+  {
+    id: pleseGiveMeAnId(),
+    name: "Alex Kidd in Miracle World",
+    category: "Precision Platform",
+    year: 1991,
+    imgUrl: 'https://www.sega-brasil.com.br/fullalbums/jogos/Master%20System/Caixas%20de%20Papelao%20Branca/Alex%20Kidd%20in%20Miracle%20World/alexkiddinmiracleworld_ft_zfm_sls.png',
     havePlay: true,
     rating: 10
   }
